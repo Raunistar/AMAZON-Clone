6ab5e861d708a4c1795820d25f0a1f3828bcd48a
Raunistar <raunistar001@gmail.com>
2025-08-19 19:14:25 +0530

    learn promises and covert code to promises

--- data/checkout.js
+++ data/checkout.js
@@ -2,7 +2,13 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 // import "../backend/backend-practice.js";
-loadProducts(() => {
+
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve("value1");
+  });
+
+}).then((value) => {
   renderOrderSummary();
   renderPaymentSummary();
 });
